860771b1e5b253ba581273ecf685c33315f67dae
Kristian Harju <harjuko@gmail.com>
2019-02-09 17:46:46 +0000

    continued

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="">
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
